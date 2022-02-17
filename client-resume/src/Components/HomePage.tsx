0861260105a6cc280ac43171380a0fae4e9a40c6
AndreiAntanovich <aandrayko@gmail.com>
2022-02-17 16:00:42 +0300

    first settings and node-sass

---
+++ client-resume/src/Components/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomePage.scss';
+
+const  HomePage:React.FunctionComponent = () =>{
+  return (
+    <div className="container-homePage">
+        Hello kdsfodsmkflmdsklfmsdkfmd
+    </div>
+  );
+}
+
+export default HomePage;
